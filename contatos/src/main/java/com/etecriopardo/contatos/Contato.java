da9addc3b69098d84e725ee8098f37cf478dfc5b
KauaBA07 <kaua.amaral2@etec.sp.gov.br>
2024-09-23 15:07:47 -0300

    Add files via upload

---
+++ contatos/src/main/java/com/etecriopardo/contatos/Contato.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.etecriopardo.contatos;
+
+/**
+ *
+ * @author Etec
+ */
+public class Contato {
+    /*VOCÊ DEVE DEFINIR O CONTEÚDO DA CLASSE DE ACORDO COM O DIAGRAMA PASSADO
+    PELO PROFESSOR
+    */
+    private String nome;
+    private String sobrenome;
+    private String telefone;
+    private String email;
+    private String categoria;
+
+    public Contato(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nome:"+this.getNome()+"\t Sobrenome:"+this.getSobrenome()+"\n"+
+                "Telefone:"+this.getTelefone()+"\t E-mail:"+this.getEmail()+"\n"+
+                "Categoria:"+this.getCategoria();
+             }
+    
+}
